e9fdda5ed29f45f90680a30e5f8616dae6c4b592
Rabat Chowdhury <hrcrabat@gmail.com>
2020-03-25 21:28:12 +0600

    Bangladesh map with district name labeled

--- src/components/BangladeshMap.js
+++ src/components/BangladeshMap.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { feature, mesh } from 'topojson-client';
-// import { select, path } from 'd3';
 import { geoMercator, geoPath, scaleLinear } from 'd3-geo';
 
 import bd from '../bd.topo.json';
-import { districts } from '../districts.json';
+import districts from '../districts.json';
 
 const features = feature(bd, bd.objects.collection);
 const meshes = mesh(bd, bd.objects.collection);
@@ -12,72 +11,28 @@ const meshes = mesh(bd, bd.objects.collection);
 const BangladeshMap = () => {
   const projection = geoMercator()
     .center([91.2, 24.8])
-    .scale(5800);
+    .scale(6100);
 
   const pathGenerator = geoPath().projection(projection);
-
-  // console.log(districts);
-  // console.log(features);
-
-  const dist = features.features.map(p => {
-    const districtName = p.properties.DISTNAME;
-
-    const x = districts.find(d => {
-      return d.name.toLowerCase() === districtName.toLowerCase();
-    });
-
-    console.log(districtName, x);
-
-    return {
-      districtName
-    };
-  });
-
-  // console.log(dist);
 
   return (
     <svg className="w-full h-full">
       <path fill="#EAEAEA" d={pathGenerator(features)}></path>
-      <path fill="none" stroke="#DDD" d={pathGenerator(meshes)}></path>
+      <path fill="none" stroke="#DDD" d={pathGenerator(meshes)}></path>$
+      {districts.map(({ district, long, lat }, idx) => {
+        const [px, py] = projection([long, lat]);
+        const labelStyle = {
+          fontSize: '10px',
+          transform: `translate(${px}px, ${py}px)`
+        };
+        return (
+          <text key={idx} style={labelStyle} x="-16" y="8">
+            {district}
+          </text>
+        );
+      })}
     </svg>
   );
-
-  // ====================================
-
-  // const svgRef = useRef();
-
-  // const holderRef = useRef();
-
-  //   console.log(mapData);
-  //   console.log(features);
-
-  //   const projection = geoMercator();
-  //   const pathGenerator = geoPath().projection(projection);
-
-  // useEffect(() => {
-  //   console.log(features);
-
-  //   const svg = select(svgRef.current);
-
-  //   const projection = geoMercator();
-  //   projection.center([91.2, 24.8]);
-  //   projection.scale(6500);
-  //   projection.rotate(0, 0, 0);
-
-  //   const pathGenerator = geoPath().projection(projection);
-
-  //   svg
-  //     .selectAll('.city')
-  //     .data(features.features)
-  //     .join('path')
-  //     .attr('class', 'city')
-  //     .attr('d', feature => {
-  //       console.log(feature);
-  //       return pathGenerator(feature);
-  //     });
-  // }, [mapData]);
-
-  //
 };
 
 export default BangladeshMap;
